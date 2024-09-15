e47f23b120ce294c6830dc0f9c3ed6a10481fd58
smosco <go1ruf2tk3@gmail.com>
2024-09-15 21:49:56 +0900

    chore: disable import/no-extraneous-dependencies rule for test files

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -3,26 +3,20 @@ import React from 'react';
 import './button.css';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** What background color to use */
   backgroundColor?: string;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
 }
 
-/** Primary UI component for user interaction */
-export const Button = ({
+export function Button({
   primary = false,
   size = 'medium',
   backgroundColor,
   label,
-  ...props
-}: ButtonProps) => {
+  onClick,
+}: ButtonProps) {
   const mode = primary
     ? 'storybook-button--primary'
     : 'storybook-button--secondary';
@@ -32,7 +26,7 @@ export const Button = ({
       className={['storybook-button', `storybook-button--${size}`, mode].join(
         ' ',
       )}
-      {...props}
+      onClick={onClick}
     >
       {label}
       <style jsx>{`
@@ -42,4 +36,4 @@ export const Button = ({
       `}</style>
     </button>
   );
-};
+}
