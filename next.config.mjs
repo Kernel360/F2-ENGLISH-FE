2b08561bd209c91d3de686e1d261f34f9b762208
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-01 10:52:10 +0900

    feat : rewrites Rule을 이용하여 proxy 서버 세팅 (#27)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['media.cnn.com'], // 외부 이미지 도메인 추가
   },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.238.253.88:8080/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
