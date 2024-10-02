a5a1d1b2ba3f2e3c70761456f378f2d46b71c545
smosco <go1ruf2tk3@gmail.com>
2024-10-02 16:48:21 +0900

    feat: Carousel 컴포넌트를 사용해 인기 리스닝, 리딩 컨텐츠 보여주도록 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>English Learning Site</h1>;
+import Carousel from '@/components/Carousel';
+import readingList from '../mock/readingList.json';
+import listeningList from '../mock/listeningList.json';
+
+export default function HomePage() {
+  return (
+    <div className="max-w-[1440px] mx-auto flex flex-col gap-12">
+      {/* 인기 리스닝 컨텐츠 캐러셀 */}
+      <Carousel
+        title="인기 리딩 컨텐츠"
+        data={readingList}
+        type="reading"
+        itemWidth={320}
+        itemsPerPage={3}
+      />
+      {/* 인기 리딩 컨텐츠 캐러셀 */}
+      <Carousel
+        title="인기 리스닝 컨텐츠"
+        data={listeningList}
+        type="listening"
+        itemWidth={320}
+        itemsPerPage={3}
+      />
+    </div>
+  );
 }
