7b32513f1fe02f800421335dc5e1a7064a385d5b
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-02 13:22:47 +0900

    feat : 영어 자막 문단으로 구현

---
+++ src/app/learn/reading/detail/[id]/page.tsx
@@ -0,0 +1,66 @@
+// detailPage에서는 해당 페이지의 contentId로 데이터를 fetch하여 보여준다
+
+// const fetchDataByPageId = () => {
+//   Response.json();
+// };
+
+import Data from '@/mock/readingDetailData.json';
+import Image from 'next/image';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
+
+export default function DetailReadingPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className="flex flex-col items-center px-[10%]  md:mb-[1.5rem] max-w-[1024px] mb-[2.5rem]">
+      <div className="flex flex-col gap-2 my-3 justify-between items-start w-full">
+        <Badge>카테고리</Badge>
+        <div className="font-bold text-3xl  mt-2 mb-4">{Data.title}</div>
+        <div className="flex justify-end w-full">조회수</div>
+        {/* 받는 Data 이름이 thumbnail이면 안 됨  */}
+      </div>
+
+      <Separator className="" />
+      <div className="my-3">
+        <Image
+          src={Data.thumbnail}
+          width={600}
+          height={400}
+          alt="이미지"
+          className="rounded-lg"
+        />
+      </div>
+      {/* 한글 있는 버전도 만들어 넣기 */}
+      <ul className="overflow-auto mb-8 w-[800px]  tracking-normal leading-loose">
+        {/* 5문장 기준으로 한 문단으로 묶어서 보여주기 */}
+        {Data.scripts.map((script, index) =>
+          // 한 문단의 첫 번째 문장에 indentation 주기
+          // eslint-disable-next-line no-nested-ternary
+          (index + 1) % 5 === 1 ? (
+            <li key={index} className="inline">
+              &nbsp;&nbsp;&nbsp;&nbsp;{script.enScript}
+            </li>
+          ) : // 한 문단의 마지막 문장에 줄바꿈 추가
+          (index + 1) % 5 === 0 ? (
+            <li key={index} className="inline">
+              {script.enScript}
+
+              <br />
+              <br />
+            </li>
+          ) : (
+            // 아무것도 적용되지 않는 문장
+            <li key={index} className="inline">
+              {script.enScript}
+            </li>
+          ),
+        )}
+      </ul>
+      <div>플로팅 북마크 버튼</div>
+      <div>플로팅 위로 올리기 버튼</div>
+    </div>
+  );
+}
