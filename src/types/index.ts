d9f2e030d3ea01a317fcd727fa0a3e7283611c07
smosco <go1ruf2tk3@gmail.com>
2024-10-02 17:42:01 +0900

    refactor: 공통 타입 분리

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface ListeningPreview {
+  id: number;
+  title: string;
+  category: string;
+  imageUrl: string;
+}
+
+export interface ReadingPreview {
+  id: number;
+  title: string;
+  category: string;
+  thumbnail: string;
+  description: string;
+}
