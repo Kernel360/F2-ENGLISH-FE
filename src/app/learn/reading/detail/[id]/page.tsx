5cb789af457377a450e49a8680196309b88f3a0d
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-03 23:01:16 +0900

    feat : 문장 북마크 하면 문장 북마크하면 사이드바에 저장됨, 문장북마크여부에 따라 버튼 +,-로 변함

--- src/app/learn/reading/detail/[id]/page.tsx
+++ src/app/learn/reading/detail/[id]/page.tsx
@@ -1,13 +1,3 @@
-// const fetchDataByPageId = () => {
-// todo : detailPage에서는 해당 페이지의 contentId로 데이터를 fetch하여 보여준다
-//   Response.json();
-// };
-
-// todo
-// 1. script받아서 보여주는 부분 ui 디벨롭 필요
-// 2. script받아서 보여주는 부분 한 문장으로 할지, 문단으로 할지
-// 3. 한글 자막 보여줄 때 hover로 할지 그냥 button만 누르면 보이게 할지
-
 'use client';
 
 import Data from '@/mock/readingDetailData.json';
@@ -15,37 +5,39 @@ import Image from 'next/image';
 import { Badge } from '@/components/ui/badge';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
-import { ArrowUp, Languages, Plus } from 'lucide-react';
+import { ArrowUp, Languages, Plus, Minus } from 'lucide-react';
 import { useState } from 'react';
 import ResponsiveSidebar from '@/components/ResponsiveSidebar';
 
 export default function DetailReadingPage() {
-  // {
-  //   // params,
-  //   // 이전 페이지에서 Link로 넘어온 params를 받아오는 부분인데 이 페이지에서 어떻게 사용해야 할지 모르겠음
-  // }: {
-  //   // params: { id: string };
-  // },
-
   const [showTranslate, toggleShowTranlation] = useState(true);
   const toggleTranslation = () => {
     toggleShowTranlation(!showTranslate);
   };
 
+  // 북마크된 문장을 관리하는 배열
+  const [bookMarkedSentences, setBookMarkedSentences] = useState<string[]>([]);
+
+  const toggleBookmarking = (sentence: string) => {
+    setBookMarkedSentences((prevBookmarkedSentences) => {
+      if (prevBookmarkedSentences.includes(sentence)) {
+        return prevBookmarkedSentences.filter((s) => s !== sentence);
+      }
+
+      return [...prevBookmarkedSentences, sentence];
+    });
+  };
+
   return (
     <>
       <div className="flex">
-        {/* 부모요소 flex, 자식요소 중 중심이 되고싶은 컨텐츠에 flex-1 넣으면 shirnk 시에 최대한 걔 유지하고 나머지 자식요소가 줄어듦  */}
         <div className="flex flex-col flex-1 gap-5 mx-auto p-5 pb-[60px] md:pb-[0px] max-w-[800px]">
-          {/* 제목 부분 */}
           <div>
             <Badge>카테고리</Badge>
             <div className="font-bold text-3xl mt-2 mb-4">{Data.title}</div>
             <div className="flex justify-end w-full">조회수</div>
           </div>
-          {/* 구분선 */}
           <Separator />
-          {/* 이미지 */}
           <div className="flex justify-center">
             <Image
               src={Data.thumbnail}
@@ -56,17 +48,28 @@ export default function DetailReadingPage() {
             />
           </div>
           <div>
-            {/* 1문장씩 영/한 보여줌 */}
             <ul className="overflow-auto tracking-normal leading-loose">
               {Data.scripts.map((script, index) => (
                 <li
+                  // eslint-disable-next-line react/no-array-index-key
                   key={index}
                   className="bg-white rounded shadow-mille relative group"
                 >
                   <p>{script.enScript}</p>
                   {showTranslate && <p>{script.koScript}</p>}
-                  <Button className="absolute left-0 -bottom-4 transform -translate-y-1/2 bg-blue-500 text-white p-1 rounded-full opacity-0 group-hover:opacity-100 transition-opacity w-8 h-8 flex items-center justify-center">
-                    <Plus className="w-4 h-4" />
+                  <Button
+                    onClick={() => toggleBookmarking(script.enScript)} // 문장 전달
+                    className={`absolute left-0 -bottom-4 transform -translate-y-1/2 p-1 rounded-full w-8 h-8 flex items-center justify-center transition-opacity ${
+                      bookMarkedSentences.includes(script.enScript)
+                        ? 'bg-green-500 text-white opacity-0 group-hover:opacity-100'
+                        : 'bg-blue-500 text-white opacity-0 group-hover:opacity-100'
+                    }`}
+                  >
+                    {bookMarkedSentences.includes(script.enScript) ? (
+                      <Minus className="w-4 h-4" />
+                    ) : (
+                      <Plus className="w-4 h-4" />
+                    )}
                   </Button>
                   <br />
                 </li>
@@ -75,7 +78,6 @@ export default function DetailReadingPage() {
           </div>
         </div>
 
-        {/* floating 버튼 */}
         <div className="fixed right-[60px] bottom-[76px] md:bottom-[16px]">
           <Button
             onClick={toggleTranslation}
@@ -98,7 +100,10 @@ export default function DetailReadingPage() {
           </Button>
         </div>
       </div>
-      <ResponsiveSidebar />
+      <ResponsiveSidebar
+        bookMarkedSentences={bookMarkedSentences}
+        toggleBookmarking={toggleBookmarking}
+      />
     </>
   );
 }
--- src/components/ResponsiveSidebar.tsx
+++ src/components/ResponsiveSidebar.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { PanelRightClose, PanelRightOpen } from 'lucide-react';
+import { PanelRightClose, PanelRightOpen, Minus } from 'lucide-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
-export default function ResponsiveSidebar() {
+interface ResponsiveSidebarProps {
+  bookMarkedSentences: string[];
+  toggleBookmarking: (sentence: string) => void;
+}
+export default function ResponsiveSidebar({
+  bookMarkedSentences,
+  toggleBookmarking,
+}: ResponsiveSidebarProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
@@ -13,17 +20,31 @@ export default function ResponsiveSidebar() {
     <>
       {/* 사이드바 */}
       <div
-        className={`fixed top-20 right-0 h-[30%] transform transition-transform duration-300 ease-in-out z-40  bg-gray-200  rounded-lg
+        className={`fixed top-20 right-0 h-[30%] w-[30%] transform transition-transform duration-300 ease-in-out z-40  bg-gray-200  rounded-lg
         ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'}
        xl:w-[300px] `}
       >
-        {/* todo : drag & drop으로 위치 변경 가능하게 만들기 */}
-        {/* 모바일 화면이면 30%민 보이도록 */}
+        {/* todo : drag & drop으로 위치 변경 가능하게 만들기? or 배경화면 투명하게 */}
+        {/* todo : 모바일 화면이면 30%민 보이도록 */}
         {/* 사이드바 콘텐츠 */}
-        <div className="p-4">
-          <h2>Sidebar Content</h2>
-          {/* 추가 콘텐츠 */}
-        </div>
+        <ScrollArea className="h-[100%] w-[100%] p-4 scrollbar">
+          <h2>저장한 문장</h2>
+          {bookMarkedSentences.map((sentence, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <div>
+              <Button
+                onClick={() => toggleBookmarking(sentence)}
+                className="w-4 h-4 p-1 mr-1"
+              >
+                <Minus />
+              </Button>
+              {/* eslint-disable-next-line react/no-array-index-key */}
+              <li key={index} className="inline-block line-clamp-2">
+                {sentence}
+              </li>
+            </div>
+          ))}
+        </ScrollArea>
       </div>
 
       {/* 화면이 작을 때만 버튼이 보임 */}
