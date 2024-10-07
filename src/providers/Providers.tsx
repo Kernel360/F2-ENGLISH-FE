e68cbb304aca68903a32bbece466fb9d1445fa6b
smosco <go1ruf2tk3@gmail.com>
2024-10-07 18:34:24 +0900

    feat: tanstack query provider 생성 및 layout에 적용

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
