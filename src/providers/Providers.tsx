04ea213555088b1d1708cc818a97be3ba3c78c5f
smosco <go1ruf2tk3@gmail.com>
2024-10-12 20:32:20 +0900

    chore: tanstack query devtools 임시 제거

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,44 +1,31 @@
 'use client';
 
-// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
+const queryClientOptions = {
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
     },
-  });
-}
+  },
+};
 
 let browserQueryClient: QueryClient | undefined;
 
 function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient();
+  if (typeof window === 'undefined') {
+    // 서버에서는 항상 새로운 QueryClient 생성
+    return new QueryClient(queryClientOptions);
   }
-  // Browser: make a new query client if we don't already have one
-  // This is very important, so we don't re-make a new client if React
-  // suspends during the initial render. This may not be needed if we
-  // have a suspense boundary BELOW the creation of the query client
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+
+  // 브라우저에서는 기존 클라이언트를 재사용하거나 새로 생성
+  if (!browserQueryClient) {
+    browserQueryClient = new QueryClient(queryClientOptions);
+  }
   return browserQueryClient;
 }
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
   const queryClient = getQueryClient();
 
   return (
