aa2272ab185244a8189326375a308f9d4aa058e3
smosco <go1ruf2tk3@gmail.com>
2024-10-13 14:04:33 +0900

    feat: 리스닝 학습 페이지 api 호출해서 뿌려주도록 수정

--- src/app/learn/listening/page.tsx
+++ src/app/learn/listening/page.tsx
@@ -1,19 +1,39 @@
-'use Client';
+'use client';
 
-import listeningList from '@/mock/listeningList.json';
-// import ContentsCard from '@/components/ContentsCard';
 import ListeningPreviewCard from '@/components/ListeningPreviewCard';
+import { useListeningContents } from '@/api/hooks/usePreview';
 
-function listeningPage() {
+function ListeningPage() {
+  const {
+    data: listeningContents,
+    isLoading,
+    isError,
+    error,
+  } = useListeningContents();
+
+  if (isLoading) {
+    return <p className="mt-8">로딩 중...</p>;
+  }
+
+  if (isError) {
+    return (
+      <p className="mt-8 text-red-500">에러가 발생했습니다: {error.message}</p>
+    );
+  }
+
+  if (!listeningContents || listeningContents.data.contents.length === 0) {
+    return <p className="mt-8">콘텐츠가 없습니다.</p>;
+  }
+
   return (
-    <main className="max-w-[1440px] mx-auto px-6 py-8">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m">
-        {listeningList.map((card) => (
-          <ListeningPreviewCard key={card.contentId} data={card} />
+    <main className="mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        {listeningContents.data.contents.map((content) => (
+          <ListeningPreviewCard key={content.contentId} data={content} />
         ))}
       </div>
     </main>
   );
 }
 
-export default listeningPage;
+export default ListeningPage;
--- src/app/learn/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function learnPage() {
-  return <div className="max-w-[1440px] mx-auto">학습 페이지</div>;
-}
