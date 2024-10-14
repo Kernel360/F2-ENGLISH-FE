fec4d31c51214213d38a9209fe4fef782aee6b4a
smosco <go1ruf2tk3@gmail.com>
2024-10-14 17:21:41 +0900

    Quiz 컴포넌트에서 문제 유형에 따라 다른 컴포넌트 보여주도록 수정

---
+++ src/components/quiz/OrderQuiz.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect, useCallback } from 'react';
+import { Button } from '../ui/button';
+
+interface OrderQuizProps {
+  questionKo: string;
+  question: string;
+  answer: string;
+  onSubmit: (isCorrect: boolean) => void;
+}
+
+export default function OrderQuiz({
+  questionKo,
+  question,
+  answer,
+  onSubmit,
+}: OrderQuizProps) {
+  const [selectedWords, setSelectedWords] = useState<string[]>([]);
+  const [remainingWords, setRemainingWords] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSelectedWords([]);
+    setRemainingWords(question.split(' '));
+  }, [question]);
+
+  const handleClickSelect = useCallback((word: string) => {
+    setSelectedWords((prev) => [...prev, word]);
+    setRemainingWords((prev) =>
+      prev.filter((existingWord) => existingWord !== word),
+    );
+  }, []);
+
+  const handleClickUnselect = useCallback((word: string) => {
+    setRemainingWords((prev) => [...prev, word]);
+    setSelectedWords((prev) =>
+      prev.filter((existingWord) => existingWord !== word),
+    );
+  }, []);
+
+  const handleClickSubmit = useCallback(() => {
+    const submittedAnswer = selectedWords.join(' ');
+    const isCorrect = submittedAnswer === answer;
+    onSubmit(isCorrect);
+  }, [selectedWords, answer, onSubmit]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p>{questionKo}</p>
+      {/* Selected words */}
+      <div className="flex flex-wrap gap-2 border-b min-h-8">
+        {selectedWords.map((word, index) => (
+          <Button
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            variant="ghost"
+            size="sm"
+            onClick={() => handleClickUnselect(word)}
+          >
+            {word}
+          </Button>
+        ))}
+      </div>
+      {/* Remaining words */}
+      <div className="flex flex-wrap gap-2">
+        {remainingWords.map((word, index) => (
+          <Button
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            variant="outline"
+            size="sm"
+            onClick={() => handleClickSelect(word)}
+          >
+            {word}
+          </Button>
+        ))}
+      </div>
+      <Button className="mt-auto" onClick={handleClickSubmit}>
+        제출
+      </Button>
+    </div>
+  );
+}
