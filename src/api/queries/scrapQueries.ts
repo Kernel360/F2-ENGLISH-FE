aab01280bf6f7d5bf3f8e056abd0a2b3ffa9d17d
smosco <go1ruf2tk3@gmail.com>
2024-10-14 12:12:33 +0900

    feat: 콘텐츠가 스크랩되었는지 확인하는 api 요청 훅 추가

--- src/api/queries/scrapQueries.ts
+++ src/api/queries/scrapQueries.ts
@@ -2,13 +2,14 @@ import {
   FetchScrapResponse,
   CreateScrapResponse,
   DeleteScrapResponse,
+  CheckScrapResponse,
 } from '@/types/Scrap';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/scrap/view`;
+const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/scrap`;
 
 // 스크랩 조회
 export const fetchScrap = async (): Promise<FetchScrapResponse> => {
-  const response = await fetch(BASE_URL, {
+  const response = await fetch(`${BASE_URL}/view`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
@@ -19,11 +20,27 @@ export const fetchScrap = async (): Promise<FetchScrapResponse> => {
   return response.json();
 };
 
+// 스크랩 되었는지 확인
+export const checkScrap = async (
+  contentId: number,
+): Promise<CheckScrapResponse> => {
+  const response = await fetch(`${BASE_URL}/check?contentId=${contentId}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch scrap');
+  }
+  return response.json();
+};
+
 // 스크랩 생성
 export const createScrap = async (
   contentId: number,
 ): Promise<CreateScrapResponse> => {
-  const response = await fetch(BASE_URL, {
+  const response = await fetch(`${BASE_URL}/create`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
@@ -39,9 +56,11 @@ export const createScrap = async (
 export const deleteScrap = async (
   contentId: number,
 ): Promise<DeleteScrapResponse> => {
-  const response = await fetch(`${BASE_URL}?contentId=${contentId}`, {
+  const response = await fetch(`${BASE_URL}/delete`, {
     method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
+    body: JSON.stringify({ contentId }),
   });
   if (!response.ok) {
     throw new Error('Failed to delete scrap');
