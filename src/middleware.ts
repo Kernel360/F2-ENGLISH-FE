294bd7e0da4dcfc575058ab7697aad2b901efc96
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-16 11:53:09 +0900

    feat : 스크랩페이지는 disabled modal띄우는 것으로 페이지액세스권한 과정 UI 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,14 @@
-// import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-// This function can be marked `async` if using `await` inside
 
 export function middleware(request: NextRequest) {
-  // 서버컴포넌트에서는 쿠키 접속 가능한지 확인 -> 가능!
-  //   const test = cookies();
-  //   console.log('============================test', test);
-  return NextResponse.redirect(new URL('/login', request.url));
+  // 서버컴포넌트에서는 쿠키 접근 가능
+  const token = request.cookies.get('access_token');
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/mypage/:path*', '/scrapbook/:path*'],
+  matcher: ['/mypage/:path*'],
 };
