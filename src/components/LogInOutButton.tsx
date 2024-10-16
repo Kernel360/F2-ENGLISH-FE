3b5c3fe15d38accfb0158a0db52f2d35487b4bf2
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-16 20:42:39 +0900

    feat : oauth로그인할때, 로그인 버튼 누를 때의 페이지 pathname보내는 로직. 이 로직으로 로그인 완료후 로그인 전 페이지로 되돌아올 수 있습니다.

--- src/components/LogInOutButton.tsx
+++ src/components/LogInOutButton.tsx
@@ -4,15 +4,17 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useUserLoginStatus } from '@/api/hooks/useUserInfo'; // custom hook import
 import { useRequestLogout } from '@/api/hooks/useUserInfo'; // custom hook import
+import { usePathname } from 'next/navigation';
 
 export default function LogInOutButton() {
   const { data: isLogin } = useUserLoginStatus();
   const { mutate: fetchUserLogout } = useRequestLogout();
+  const currentPathname = usePathname();
 
   return isLogin?.data ? (
     <Button onClick={() => fetchUserLogout()}>로그아웃</Button>
   ) : (
-    <Link href="/login">
+    <Link href={`/login?returnUrl=${currentPathname}`}>
       <Button>로그인</Button>
     </Link>
   );
