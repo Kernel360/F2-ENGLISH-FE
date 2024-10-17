70050bbee20c6a4604f163289207bd859cc96273
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-17 13:09:52 +0900

    design : 이미지 placeholder 회색 이미지로 변경

--- src/app/(default)/mypage/profile/page.tsx
+++ src/app/(default)/mypage/profile/page.tsx
@@ -70,7 +70,7 @@ export default function UserProfile() {
         <div className="flex justify-center mb-6">
           <div className="relative">
             <img
-              src="https://avatar.iran.liara.run/public/girl"
+              src={`https://catalystcci.com/wp-content/uploads/gray-image-placeholder.png`}
               alt="Profile"
               className="w-20 h-20 rounded-full"
             />
